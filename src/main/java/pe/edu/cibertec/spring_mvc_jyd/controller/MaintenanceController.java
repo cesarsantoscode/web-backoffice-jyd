c8387b7c15db839bc179b4751bf8d691e7872496
César Santos <cesarsantostorres@gmail.com>
2024-11-29 22:00:25 -0500

    Implementación de mvc

--- src/main/java/pe/edu/cibertec/spring_mvc_jyd/controller/MaintenanceController.java
+++ src/main/java/pe/edu/cibertec/spring_mvc_jyd/controller/MaintenanceController.java
@@ -3,17 +3,21 @@ package pe.edu.cibertec.spring_mvc_jyd.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.cibertec.spring_mvc_jyd.dto.FilmDto;
 import pe.edu.cibertec.spring_mvc_jyd.service.MaintenanceService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/maintenance")
 public class MaintenanceController {
 
     @Autowired
     MaintenanceService maintenanceService;
 
+    @GetMapping("/start")
     public String start(Model model) {
         List<FilmDto> films = maintenanceService.findAllFilms();
         model.addAttribute("films", films);
